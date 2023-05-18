4210d8da3a6f843cd2115d9c713011793021d8f2
Bryan Valerin <bryanvalerin@gmail.com>
2023-05-18 01:21:18 -0600

    Mejoras SEO

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports.SITE = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'Asystec - Soluciones de Software',
-  description: '¡Impulsa la eficiencia y el crecimiento de tu negocio con nuestro software de facturación y gestión empresarial! Somos una empresa líder en tecnología que ofrece soluciones personalizadas para empresas de todos los sectores. Descubre cómo nuestra plataforma te ayuda a simplificar tus procesos, aumentar la productividad y optimizar tus operaciones. ¡Maximiza tus resultados con nuestro software confiable y de vanguardia!',
+  title: 'Asystec - Potencia tu Negocio con Soluciones de Software Avanzadas',
+  description: '¡Impulsa la eficiencia y el crecimiento de tu negocio con nuestro software de facturación y gestión empresarial! Somos una empresa líder en tecnología que ofrece soluciones personalizadas para empresas de todos los sectores.',
 };
