38d8669945d28f837b3530ef210a9d038ab59cf0
Bryan Valerin <bryanvalerin@gmail.com>
2023-05-18 01:16:54 -0600

    se amplia etiquetas meta

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports.SITE = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'Asystec',
-  description: 'Somos una empresa de tecnología, proveedores de softare de facturacion y gestión empresarial para negocios de todo tipo.',
+  title: 'Asystec - Soluciones de Software',
+  description: '¡Impulsa la eficiencia y el crecimiento de tu negocio con nuestro software de facturación y gestión empresarial! Somos una empresa líder en tecnología que ofrece soluciones personalizadas para empresas de todos los sectores. Descubre cómo nuestra plataforma te ayuda a simplificar tus procesos, aumentar la productividad y optimizar tus operaciones. ¡Maximiza tus resultados con nuestro software confiable y de vanguardia!',
 };
